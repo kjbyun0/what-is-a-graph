8e6cd29222bbbc7e53b083a6bc2e9deae9037671
KI JONG BYUN <kjbyun0@gmail.com>
2024-09-07 17:32:20 -0500

    Add my solution

--- javascript/graph.js
+++ javascript/graph.js
@@ -1,15 +1,67 @@
 class Graph {
   constructor(paths) {
+    // this.graph = {};
 
+    // if (paths.length === 1 && paths[0].length === 1) {
+    //   this.graph[paths[0][0]] = new Set();
+    //   return;
+    // }
+
+    // for (const path of paths) {
+    //   for (let i = 0; i < path.length - 1; i++) {
+    //     const v1 = path[i];
+    //     const v2 = path[i+1];
+    //     if (!this.graph[v1])
+    //       this.graph[v1] = new Set([v2]);
+    //     else
+    //       this.graph[v1].add(v2);
+    //     if (!this.graph[v2])
+    //       this.graph[v2] = new Set([v1]);
+    //     else
+    //       this.graph[v2].add(v1);
+    //   }
+    // }
+
+    this.graph = paths.reduce((graph, path) => this.constructor.populate(graph, path), {});
+  }
+
+  static populate(graph, path) {
+    return path.reduce((graph, v1, i) => {
+      const v2 = path[i+1];
+      graph[v1] = graph[v1] || new Set();
+      if (v2) {
+        graph[v1].add(v2);
+        graph[v2] = graph[v2] || new Set([]);
+        graph[v2].add(v1);
+      }
+
+      return graph;
+    }, graph);
   }
 
   isAdjacent(vertexA, vertexB) {
-
+    return this.graph[vertexA].has(vertexB);
   }
 
   // array is an adjacency list
   addVertex(vertex, array) {
+    // for (const vertex2 of array) {
+    //   if (!this.graph[vertex])
+    //     this.graph[vertex] = new Set([vertex2]);
+    //   else
+    //     this.graph[vertex].add(vertex2);
+    //   if (!this.graph[vertex2])
+    //     this.graph[vertex2] = new Set([vertex]);
+    //   else
+    //     this.graph[vertex2].add(vertex);
+    // }
 
+    this.graph[vertex] = new Set(array);
+    array.reduce((graph, v) => {
+      graph[v] = graph[v] || new Set([]);
+      graph[v].add(vertex);
+      return graph;
+    }, this.graph);
   }
 }
 
